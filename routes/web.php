6aaf6cd0ff3da2abc2f7ccd34b71115f4c07bc6b
Seannix0606 <Seanarcher0606@gmail.com>
2025-10-17 14:06:15 +0800

    Add debug info to storage route

--- routes/web.php
+++ routes/web.php
@@ -262,8 +262,21 @@ Route::get('/debug', function () {
 Route::get('/storage/{path}', function ($path) {
     $fullPath = storage_path('app/public/' . $path);
     
+    // Debug info
+    if (request()->has('debug')) {
+        return response()->json([
+            'path' => $path,
+            'fullPath' => $fullPath,
+            'exists' => file_exists($fullPath),
+            'is_file' => is_file($fullPath),
+            'is_readable' => is_readable($fullPath),
+            'size' => file_exists($fullPath) ? filesize($fullPath) : 'N/A',
+            'mime' => file_exists($fullPath) ? mime_content_type($fullPath) : 'N/A'
+        ]);
+    }
+    
     if (!file_exists($fullPath)) {
-        abort(404);
+        abort(404, "File not found: $fullPath");
     }
     
     $mimeType = mime_content_type($fullPath);
