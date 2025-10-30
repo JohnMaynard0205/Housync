1022fa80ea357ba79f161d4d5ba744ef7682a9e8
Seannix0606 <Seanarcher0606@gmail.com>
2025-10-31 02:12:29 +0800

    fix(landlord): show landlordProfile->name in header; fallback to user name

--- resources/views/layouts/landlord-app.blade.php
+++ resources/views/layouts/landlord-app.blade.php
@@ -246,8 +246,13 @@
         <div class="topbar">
             <div class="dropdown" id="llProfileDropdown">
                 <div class="profile-btn" id="llProfileBtn">
-                    <div class="profile-avatar">{{ substr(auth()->user()->name, 0, 1) }}</div>
-                    <span class="d-none d-sm-inline">{{ auth()->user()->name }}</span>
+                    @php
+                        $llUser = auth()->user();
+                        $displayName = optional($llUser->landlordProfile)->name ?? $llUser->name ?? 'Landlord';
+                        $initial = mb_substr($displayName, 0, 1);
+                    @endphp
+                    <div class="profile-avatar">{{ $initial }}</div>
+                    <span class="d-none d-sm-inline">{{ $displayName }}</span>
                     <i class="fas fa-chevron-down" style="font-size:.85rem;color:#64748b"></i>
                 </div>
                 <div class="dropdown-menu" id="llDropdownMenu">
