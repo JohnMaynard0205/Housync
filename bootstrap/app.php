9e097d619f8c61ae03eb48dc5b10dfee6d8ac7ac
jcalebbb <jcalebdelosreyes@gmail.com>
2025-12-03 00:01:13 +0800

    Fix mixed content issues by trusting proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'role' => \App\Http\Middleware\RoleMiddleware::class,
-        ]);
+    ->withMiddleware(function (Middleware $middleware) {
+        // ADD THIS LINE:
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
